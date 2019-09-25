5009d4b27f10e98ecf2a44823f4bac19dc62893b
Sosisko <54500448+Sosisko@users.noreply.github.com>
2019-09-25 23:03:52 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,10 @@ function getQuantityElements(heightElement) {
 
 function startGame(){
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+    car.style.left = '125px';
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
 for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
@@ -58,6 +61,8 @@ for (let i = 0; i < getQuantityElements(100); i++) {
 function playGame(){
     
     if (setting.start){
+        setting.score += setting.speed;
+        score.textContent = setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
@@ -105,6 +110,17 @@ function moveRoad() {
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item){
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left && 
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+                setting.start = false;
+                start.classList.remove('hide');
+                start.style.top = score.offsetHeight;
+
+        }
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y >= document.documentElement.clientHeight){
